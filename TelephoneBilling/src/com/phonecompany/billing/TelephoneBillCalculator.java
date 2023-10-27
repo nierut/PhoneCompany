8aeb971407a76910626c5c7cbf32ef9b281d50b3
Rút Ní Eithir <rut.nieithir@hotmail.com>
2023-10-27 13:47:07 +0200

    completes interface

--- TelephoneBilling/src/com/phonecompany/billing/TelephoneBillCalculator.java
+++ TelephoneBilling/src/com/phonecompany/billing/TelephoneBillCalculator.java
@@ -1,5 +1,19 @@
 package com.phonecompany.billing;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+
 public interface TelephoneBillCalculator {
     BigDecimal calculate (String phoneLog);
+
+    BigDecimal calculateFirstFiveMinutes(LocalDateTime start, LocalDateTime end);
+
+    boolean timeFallsIntoInterval(LocalTime time);
+
+    String getFreeNumber(String fileName);
+
+    String getMostOftenCalledNumber(HashMap<String, Integer> calledNumbers);
+
+    LocalDateTime getDate(String dateStr);
 }
